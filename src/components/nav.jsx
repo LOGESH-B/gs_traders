edd945f843a3963c85a62cfffd3324b67372abfe
iniyan <iniyansg93@gmail.com>
2023-03-22 18:17:25 +0530

    nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,11 +1,60 @@
-
-
 import React from 'react'
+import '../components.css/nav.css'
+import { FaList, FaHome, FaInfo, FaCompressArrowsAlt } from "react-icons/fa";
+import logo from '../assets/images/nav-logo.png'
 
 function Nav() {
-  return (
-    <div>Nav</div>
-  )
+    return (
+        <>
+
+        <div className='abovenavd'>
+            <h5>Contact us</h5>
+            <small>email:gstraders@gmai.com</small>
+            <small>phno:123456789</small>
+        </div>
+
+        <div className='navfordesk'>
+            <div className='navcontent1'>
+                <img className='logo' src={logo} alt="" />
+            </div>
+            <div className='navcontent'>
+                <div className='content'>
+                     <a className='h' style={{fontSize:'medium'}}>HOME</a>
+                   <a  className='h' style={{fontSize:'medium'}}>ABOUT</a>
+               
+                
+                </div>
+                
+                <div className='button'>
+                    <bold> Book Now-</bold>
+                   </div>
+                
+
+            </div>
+
+        </div>
+        
+        <div className='abovenavm'>
+           
+        </div>
+
+        <div className='navformobile'>
+            <div className='navcontentmb'>
+                <img className='logo' src={logo} alt="" />
+            </div>
+            <div className='navcontentmb1'>
+                    <small style={{fontStyle:'italic'}}>Contact us</small>
+                    <small>email:gstraders@gmail.com</small>
+                    <small>phno:123456789</small>
+ 
+            </div>
+
+        </div>
+      
+            
+        </>
+
+    )
 }
 
 export default Nav
